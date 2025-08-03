2ec568e17d301bf0deeaae3a44e5a7c9a5824e42
Aimelive <aimendayambaje24@gmail.com>
2025-08-03 20:37:32 +0200

    initial commit

---
+++ src/main/java/com/aimelive/urutibot/UrutiBotApplication.java
@@ -0,0 +1,13 @@
+package com.aimelive.urutibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrutiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrutiBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aimelive/urutibot/UrutiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimelive.urutibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrutiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
